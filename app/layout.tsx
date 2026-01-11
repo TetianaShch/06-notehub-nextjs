69dcae3926abb14c20a7067aeb0e35744ac99f23
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-01-11 16:12:55 +0200

    feat: add TanStack Query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
 
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
@@ -28,9 +29,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
-        {children}
-        <Footer />
+        <TanStackProvider>
+          <Header />
+          {children}
+          <Footer />
+        </TanStackProvider>
       </body>
     </html>
   );
